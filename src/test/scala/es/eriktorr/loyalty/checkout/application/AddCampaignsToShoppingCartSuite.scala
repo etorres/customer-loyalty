3f20eeb6f05297c4856d1d6d21d9751520235c3e
Erik Torres <erik.torres@lifullconnect.com>
2020-10-10 21:21:34 +0200

    :sparkles: Add eligible campaigns to shopping cart

--- src/test/scala/es/eriktorr/loyalty/checkout/application/AddCampaignsToShoppingCartSuite.scala
+++ src/test/scala/es/eriktorr/loyalty/checkout/application/AddCampaignsToShoppingCartSuite.scala
@@ -10,14 +10,8 @@ import es.eriktorr.loyalty.checkout.domain.{
   ShoppingCartWithPromotions
 }
 import es.eriktorr.loyalty.checkout.infrastructure.FakeShoppingCartRepository
-import es.eriktorr.loyalty.core.domain.{
-  ItemId,
-  PromotionalOffer,
-  RewardCampaigns,
-  SomeQuantity,
-  UserId
-}
 import es.eriktorr.loyalty.core.domain.PromotionalOffer._
+import es.eriktorr.loyalty.core.domain._
 import es.eriktorr.loyalty.incentives.infrastructure.{FakeEligibilityEngine, FakeRewardCatalog}
 import eu.timepit.refined.api.Refined.unsafeApply
 import eu.timepit.refined.auto._
@@ -27,6 +21,8 @@ import squants.DimensionlessConversions._
 import squants.market.MoneyConversions._
 import weaver._
 import weaver.scalacheck._
+
+import scala.util.Random
 
 object AddCampaignsToShoppingCartSuite extends SimpleIOSuite with IOCheckers {
   implicit val showItemId: Show[TestCase] = Show.show(_.toString)
@@ -41,8 +37,8 @@ object AddCampaignsToShoppingCartSuite extends SimpleIOSuite with IOCheckers {
 
   private[this] val shoppingCartItemGen = Applicative[Gen].product(itemIdGen, quantityGen)
 
-  private[this] val promotionGen: Gen[PromotionalOffer] =
-    Gen.oneOf(
+  private[this] val promotionOffersGen = Gen.const(
+    List(
       BuyOneGetOneFree,
       BuyOneGetOneHalfPrice,
       FreeDelivery,
@@ -51,42 +47,56 @@ object AddCampaignsToShoppingCartSuite extends SimpleIOSuite with IOCheckers {
       LoyaltyPoints(SomeQuantity(100)),
       VolumeDiscount(20.0.EUR, 10.percent)
     )
+  )
 
-  private[this] def itemWithPromotionGen(itemId: ItemId, quantity: SomeQuantity) =
+  private[this] def itemWithPromotionGen(
+    itemId: ItemId,
+    quantity: SomeQuantity,
+    promotionalOffers: List[PromotionalOffer]
+  ) =
     for {
-      promotionalOffers <- Gen.nonEmptyContainerOf[List, PromotionalOffer](promotionGen)
+      promotionalOffers <- Gen.nonEmptyContainerOf[List, PromotionalOffer](
+        Gen.oneOf(promotionalOffers)
+      )
     } yield (itemId, (quantity, promotionalOffers))
 
-  private[this] def itemMaybeWithPromotionGen(itemId: ItemId, quantity: SomeQuantity) =
+  private[this] def itemMaybeWithPromotionGen(
+    itemId: ItemId,
+    quantity: SomeQuantity,
+    promotionalOffers: List[PromotionalOffer]
+  ) =
     for {
-      promotionalOffers <- Gen.containerOf[List, PromotionalOffer](promotionGen)
+      promotionalOffers <- Gen.containerOf[List, PromotionalOffer](Gen.oneOf(promotionalOffers))
     } yield (itemId, (quantity, promotionalOffers))
 
   private[this] val gen = for {
     userId <- userIdGen
     shoppingCartId <- shoppingCartIdGen
     shoppingCartItems <- Gen.containerOfN[List, (ItemId, SomeQuantity)](10, shoppingCartItemGen)
+    allPromotionOffers <- promotionOffersGen
+    (eligiblePromotionOffers, notEligiblePromotionOffers) = Random
+      .shuffle(allPromotionOffers)
+      .splitAt(3)
     (itemsWithPromotion, itemsMaybeWithPromotion) = shoppingCartItems.splitAt(3)
-    shoppingCartItemsWithPromotion <- itemsWithPromotion.traverse {
+    shoppingCartItemsWithEligiblePromotions <- itemsWithPromotion.traverse {
       case (itemId, quantity) =>
-        Gen
-          .containerOf[List, (ItemId, (SomeQuantity, List[PromotionalOffer]))](
-            itemWithPromotionGen(itemId, quantity)
-          )
+        Gen.containerOf[List, (ItemId, (SomeQuantity, List[PromotionalOffer]))](
+          itemWithPromotionGen(itemId, quantity, eligiblePromotionOffers)
+        )
     }
-    shoppingCartItemsMaybeWithPromotion <- itemsMaybeWithPromotion.traverse {
+    shoppingCartItemsMaybeWithNotEligiblePromotions <- itemsMaybeWithPromotion.traverse {
       case (itemId, quantity) =>
-        Gen
-          .containerOf[List, (ItemId, (SomeQuantity, List[PromotionalOffer]))](
-            itemMaybeWithPromotionGen(itemId, quantity)
-          )
+        Gen.containerOf[List, (ItemId, (SomeQuantity, List[PromotionalOffer]))](
+          itemMaybeWithPromotionGen(itemId, quantity, notEligiblePromotionOffers)
+        )
     }
-    eligibleShoppingCartItems = shoppingCartItemsWithPromotion.flatten
-    notEligibleShoppingCartItems = shoppingCartItemsMaybeWithPromotion.flatten
+    eligibleShoppingCartItems = shoppingCartItemsWithEligiblePromotions.flatten
+    notEligibleShoppingCartItems = shoppingCartItemsMaybeWithNotEligiblePromotions.flatten
   } yield TestCase(
     userId,
     shoppingCartId,
     eligibleShoppingCartItems,
+    notEligibleShoppingCartItems,
     eligibleShoppingCartItems ++ notEligibleShoppingCartItems
   )
 
@@ -126,7 +136,9 @@ object AddCampaignsToShoppingCartSuite extends SimpleIOSuite with IOCheckers {
         shoppingCartWithPromotions == ShoppingCartWithPromotions(
           testCase.shoppingCartId,
           testCase.userId.some,
-          testCase.eligibleShoppingCartItems.toMap
+          testCase.eligibleShoppingCartItems.toMap ++ testCase.notEligibleShoppingCartItems.map {
+            case (itemId, (quantity, _)) => (itemId, (quantity, List.empty))
+          }
         ).some
       )
     }
@@ -137,5 +149,6 @@ final case class TestCase(
   userId: UserId,
   shoppingCartId: ShoppingCartId,
   eligibleShoppingCartItems: List[(ItemId, (SomeQuantity, List[PromotionalOffer]))],
+  notEligibleShoppingCartItems: List[(ItemId, (SomeQuantity, List[PromotionalOffer]))],
   allShoppingCartItems: List[(ItemId, (SomeQuantity, List[PromotionalOffer]))]
 )
