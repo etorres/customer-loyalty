28d36a8edd34ca024dbe3fb88ce27a7b52d891b8
Erik Torres <erik.torres@lifullconnect.com>
2020-10-10 18:06:11 +0200

    :white_check_mark: Add promotional offers to the FakeEligibilityEngine

--- src/test/scala/es/eriktorr/loyalty/checkout/application/AddCampaignsToShoppingCartSuite.scala
+++ src/test/scala/es/eriktorr/loyalty/checkout/application/AddCampaignsToShoppingCartSuite.scala
@@ -2,12 +2,12 @@ package es.eriktorr.loyalty.checkout.application
 
 import cats._
 import cats.effect._
+import cats.effect.concurrent.Ref
 import cats.implicits._
 import es.eriktorr.loyalty.checkout.domain.{ShoppingCartId, ShoppingCartWithPromotions}
 import es.eriktorr.loyalty.checkout.infrastructure.FakeShoppingCartRepository
 import es.eriktorr.loyalty.core.domain.{ItemId, PromotionalOffer, SomeQuantity, UserId}
 import es.eriktorr.loyalty.core.domain.PromotionalOffer._
-import es.eriktorr.loyalty.effect._
 import es.eriktorr.loyalty.incentives.infrastructure.{FakeEligibilityEngine, FakeRewardCatalog}
 import eu.timepit.refined.api.Refined.unsafeApply
 import eu.timepit.refined.auto._
@@ -18,67 +18,72 @@ import squants.market.MoneyConversions._
 import weaver._
 import weaver.scalacheck._
 
-object AddCampaignsToShoppingCartSuite extends IOSuite with IOCheckers {
-  override type Res = AddCampaignsToShoppingCart[IO]
+object AddCampaignsToShoppingCartSuite extends SimpleIOSuite with IOCheckers {
+  implicit val showItemId: Show[TestCase] = Show.show(_.toString)
 
-  override def sharedResource: Resource[IO, Res] =
-    AddCampaignsToShoppingCart
-      .impl[IO](new FakeEligibilityEngine, new FakeRewardCatalog, new FakeShoppingCartRepository)
-      .toResource
+  private[this] val userIdGen = Gen.identifier.map(userId => UserId(unsafeApply(userId)))
 
-  test("Add eligible campaigns to shopping cart") { addCampaignsToShoppingCart =>
-    val userIdGen = Gen.identifier.map(userId => UserId(unsafeApply(userId)))
+  private[this] val shoppingCartIdGen = Gen.uuid.map(x => ShoppingCartId(unsafeApply(x.show)))
 
-    val shoppingCartIdGen = Gen.uuid.map(x => ShoppingCartId(unsafeApply(x.show)))
+  private[this] val itemIdGen = Gen.uuid.map(x => ItemId(unsafeApply(x.show)))
 
-    val itemIdGen = Gen.uuid.map(x => ItemId(unsafeApply(x.show)))
+  private[this] val quantityGen = Gen.chooseNum(1, 10).map(x => SomeQuantity(unsafeApply(x)))
 
-    val quantityGen = Gen.chooseNum(1, 10).map(x => SomeQuantity(unsafeApply(x)))
+  private[this] val shoppingCartItemGen = Applicative[Gen].product(itemIdGen, quantityGen)
 
-    val shoppingCartItemGen = Applicative[Gen].product(itemIdGen, quantityGen)
-
-    val promotionGen: Gen[PromotionalOffer] =
-      Gen.oneOf(
-        BuyOneGetOneFree,
-        BuyOneGetOneHalfPrice,
-        FreeDelivery,
-        FreeSamples(List(ItemId("dea0618e-0a67-11eb-adc1-0242ac120002"))),
-        GiftCard(10.0.EUR, SomeQuantity(1)),
-        LoyaltyPoints(SomeQuantity(100)),
-        VolumeDiscount(20.0.EUR, 10.percent)
-      )
-
-    def itemWithPromotionGen(itemId: ItemId, quantity: SomeQuantity) =
-      for {
-        promotionalOffers <- Gen.nonEmptyContainerOf[List, PromotionalOffer](promotionGen)
-      } yield (itemId, (quantity, promotionalOffers))
-
-    val gen = for {
-      userId <- userIdGen
-      shoppingCartId <- shoppingCartIdGen
-      shoppingCartItems <- Gen.containerOfN[List, (ItemId, SomeQuantity)](10, shoppingCartItemGen)
-      (itemsWithPromotion, itemsWithoutPromotion) = shoppingCartItems.splitAt(3)
-      generatedShoppingCartItemsWithPromotion <- itemsWithPromotion.traverse {
-        case (itemId, quantity) =>
-          Gen
-            .containerOf[List, (ItemId, (SomeQuantity, List[PromotionalOffer]))](
-              itemWithPromotionGen(itemId, quantity)
-            )
-      }
-      shoppingCartItemsWithPromotion = generatedShoppingCartItemsWithPromotion.flatten
-      shoppingCartItemsWithoutPromotion = itemsWithoutPromotion.map {
-        case (itemId, quantity) => (itemId, (quantity, List.empty[PromotionalOffer]))
-      }
-    } yield TestCase(
-      userId,
-      shoppingCartId,
-      shoppingCartItemsWithPromotion,
-      shoppingCartItemsWithPromotion ++ shoppingCartItemsWithoutPromotion
+  private[this] val promotionGen: Gen[PromotionalOffer] =
+    Gen.oneOf(
+      BuyOneGetOneFree,
+      BuyOneGetOneHalfPrice,
+      FreeDelivery,
+      FreeSamples(List(ItemId("dea0618e-0a67-11eb-adc1-0242ac120002"))),
+      GiftCard(10.0.EUR, SomeQuantity(1)),
+      LoyaltyPoints(SomeQuantity(100)),
+      VolumeDiscount(20.0.EUR, 10.percent)
     )
 
-    implicit val showItemId: Show[TestCase] = Show.show(_.toString)
+  private[this] def itemWithPromotionGen(itemId: ItemId, quantity: SomeQuantity) =
+    for {
+      promotionalOffers <- Gen.nonEmptyContainerOf[List, PromotionalOffer](promotionGen)
+    } yield (itemId, (quantity, promotionalOffers))
 
+  private[this] val gen = for {
+    userId <- userIdGen
+    shoppingCartId <- shoppingCartIdGen
+    shoppingCartItems <- Gen.containerOfN[List, (ItemId, SomeQuantity)](10, shoppingCartItemGen)
+    (itemsWithPromotion, itemsWithoutPromotion) = shoppingCartItems.splitAt(3)
+    generatedShoppingCartItemsWithPromotion <- itemsWithPromotion.traverse {
+      case (itemId, quantity) =>
+        Gen
+          .containerOf[List, (ItemId, (SomeQuantity, List[PromotionalOffer]))](
+            itemWithPromotionGen(itemId, quantity)
+          )
+    }
+    shoppingCartItemsWithPromotion = generatedShoppingCartItemsWithPromotion.flatten
+    shoppingCartItemsWithoutPromotion = itemsWithoutPromotion.map {
+      case (itemId, quantity) => (itemId, (quantity, List.empty[PromotionalOffer]))
+    }
+  } yield TestCase(
+    userId,
+    shoppingCartId,
+    shoppingCartItemsWithPromotion,
+    shoppingCartItemsWithPromotion ++ shoppingCartItemsWithoutPromotion
+  )
+
+  simpleTest("Add eligible campaigns to shopping cart") {
     forall(gen) { testCase =>
+      val addCampaignsToShoppingCart = AddCampaignsToShoppingCart
+        .impl[IO](
+          new FakeEligibilityEngine(
+            Ref.unsafe[IO, Map[UserId, List[PromotionalOffer]]](
+              Map(testCase.userId -> testCase.shoppingCartItemsWithPromotion.flatMap {
+                case (_, (_, promotionalOffers)) => promotionalOffers
+              })
+            )
+          ),
+          new FakeRewardCatalog,
+          new FakeShoppingCartRepository
+        )
       for {
         shoppingCartWithPromotions <- addCampaignsToShoppingCart.shoppingCartWithPromotionsFor(
           testCase.userId
--- src/test/scala/es/eriktorr/loyalty/incentives/infrastructure/FakeEligibilityEngine.scala
+++ src/test/scala/es/eriktorr/loyalty/incentives/infrastructure/FakeEligibilityEngine.scala
@@ -1,10 +1,15 @@
 package es.eriktorr.loyalty.incentives.infrastructure
 
 import cats.effect._
+import cats.effect.concurrent.Ref
 import es.eriktorr.loyalty.core.domain._
 import es.eriktorr.loyalty.incentives.domain._
 
-final class FakeEligibilityEngine extends EligibilityEngine[IO] {
+final class FakeEligibilityEngine(val ref: Ref[IO, Map[UserId, List[PromotionalOffer]]])
+    extends EligibilityEngine[IO] {
   override def isEligible(userId: UserId, promotionalOffer: PromotionalOffer): IO[Boolean] =
-    IO.pure(false)
+    for {
+      promotionalOffers <- ref.get
+      isEligible = promotionalOffers.getOrElse(userId, List.empty).contains(promotionalOffer)
+    } yield isEligible
 }
